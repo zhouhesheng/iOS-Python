0fdf632508e2e6bd5ad2759a4784cb22858e9d72
Hesheng Zhou <hzhou@ilegendsoft.com>
2014-04-16 10:54:22 +0800

    test

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 def f():
 	import encodings, yaml
 	print yaml.load("a: 1")
-	print "hello"
+	print " - - - hello"
 	foo = {"x":100000}
 	print foo
 	import sys
-	print sys.path
+	print sys.path, "---Version:", sys.version, "---System builtins", sys.builtin_module_names
